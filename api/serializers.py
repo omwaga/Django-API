7d800f1f599fe8a201165aff87e8c4991eda277c
Collins Omwaga <collinsomwaga@gmail.com>
2021-09-05 13:46:46 +0300

    serializer

--- api/serializers.py
+++ api/serializers.py
@@ -6,3 +6,14 @@ class ArticleSerializer(serializers.Serializer):
     author = serializers.CharField(max_length=100)
     email = serializers.EmailField(max_length=100)
     date = serializers.DateField()
+
+    def create(self, validated_data):
+        return Article.objects.create(validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.author = validated_data.get('author', instance.author)
+        instance.email = validated_data.get('email', instance.email)
+        instance.date = validated_data.get('date', instance.date)
+        instance.save()
+        return instance
